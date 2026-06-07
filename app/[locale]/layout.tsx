ecfaf1e9726f9c48229a9d819a934a52f179799d
MCuffs <28343094+MCuffs@users.noreply.github.com>
2026-06-07 19:37:14 +0900

    feat: 메인 화면에서 ThinkingData 추적 활성화

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -5,6 +5,7 @@ import { NextIntlClientProvider } from 'next-intl';
 import { getMessages } from 'next-intl/server';
 import { notFound } from 'next/navigation';
 import { routing } from '../../i18n/routing';
+import ThinkingDataProvider from '../components/ThinkingDataProvider';
 
 const pretendard = localFont({
   variable: "--font-pretendard",
