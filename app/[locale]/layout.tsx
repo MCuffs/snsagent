d31da67683f54c2d62ca2590fb9f10a130face4c
MCuffs <28343094+MCuffs@users.noreply.github.com>
2026-06-07 20:16:41 +0900

    feat: ThinkingData 추적 컴포넌트 렌더링 추가

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -94,6 +94,7 @@ export default async function LocaleLayout({
     <html lang={locale} className={`${pretendard.variable} h-full antialiased`}>
       <body className="min-h-full flex flex-col">
         <NextIntlClientProvider messages={messages}>
+          <ThinkingDataProvider />
           {children}
         </NextIntlClientProvider>
       </body>
